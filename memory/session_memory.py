919212640d94b415ab96f619eb55ed108ab51a8f
srikanthPyQuery <srigadiparthi95@gmail.com>
2026-02-23 16:05:02 +0530

    Create session_memory.py

---
+++ memory/session_memory.py
@@ -0,0 +1,13 @@
+import streamlit as st
+
+def initialize_memory():
+    if "chat_history" not in st.session_state:
+        st.session_state.chat_history = []
+
+def add_message(role, content):
+    st.session_state.chat_history.append(
+        {"role": role, "content": content}
+    )
+
+def get_history():
+    return st.session_state.chat_history
